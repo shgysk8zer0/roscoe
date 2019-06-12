4bcdc829667269fcd34ac79dde7ba30e269c87c3
Chris Zuber <shgysk8zer0@gmail.com>
2019-06-12 14:44:19 -0700

    Update service worker version

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 // 2019-06-11 12:25
 const config = {
-	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0',
+	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0-alpha',
 	stale: [
 		'/',
 		'/js/index.js',
